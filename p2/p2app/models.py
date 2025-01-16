2abb1b4d722a50eb6cdd8414e2193c7843b2ce17
Rana Uzair <ranauzair686@gmail.com>
2025-01-16 23:08:22 +0500

    Update models.py

--- p2/p2app/models.py
+++ p2/p2app/models.py
@@ -1,8 +1,1 @@
-from django.db import models
 
-# Create your models here.
-
-class Myinfo(models.Model):
-    name  = models.CharField(max_length=50)
-    bio   = models.TextField()
-    profile_pic = models.ImageField(upload_to='profile_pics/')
